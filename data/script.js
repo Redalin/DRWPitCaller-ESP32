4d6dd25c612fa6fc0587b80f2115c6ea51299a16
criddell-eb <39214477+Redalin@users.noreply.github.com>
2025-04-18 21:28:41 +1200

    add extra voice picker

--- data/script.js
+++ data/script.js
@@ -338,3 +338,32 @@ function getCountdownTimer() {
 function updateBGColour(section, colour) {
     document.getElementById(section).style.backgroundColor = colour;
 }
+
+function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+        return;
+    }
+
+    const voices = speechSynthesis.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+        const option = document.createElement("option");
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+        if (voices[i].default) {
+            option.textContent += " — DEFAULT";
+        }
+
+        option.setAttribute("data-lang", voices[i].lang);
+        option.setAttribute("data-name", voices[i].name);
+        document.getElementById("speechPicker").appendChild(option);
+    }
+}
+
+populateVoiceList();
+if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+}
